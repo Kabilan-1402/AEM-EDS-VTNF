d739f0470918ceffa2bcb22e8089a099ba56a118
Kabilan M <kabilan1302@gmail.com>
2026-08-21 19:43:32 +0530

    js-name -chages

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -7,9 +7,9 @@ export default function decorate(block) {
     const cardimg = card.querySelector('p:has(img)');
     cardimg?.classList.add('custom-card-image');
     const cardtitle = card.querySelector('h2');
-    cardtitle?.classList.add('custom-card-title');
+    cardtitle?.classList.add('custom-card-heading');
     const cardtext = card.querySelector('p:not(:has(img))');
-    cardtext?.classList.add('custom-card-text');
+    cardtext?.classList.add('custom-card-description');
   });
 }
 
